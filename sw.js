9ee9fb55d0b0f2c5a72c2d406c548e7c9224099e
berat-arsln <beratarsln@gmail.com>
2026-05-01 18:50:02 +0300

    Change cache name to 'passaword-v8'
    Updated cache name to version 8.

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ======================================== */
 
 /* Önbellek adı ve versiyonu */
-const ONBELLEK_ADI = 'passaword-v7';
+const ONBELLEK_ADI = 'passaword-v8';
 
 /* Önbelleğe alınacak dosyalar */
 const ONBELLEKLENECEK = [
