9251b7b56385bcdba35f4d6f90de3bf169328f00
berat-arsln <beratarsln@gmail.com>
2026-05-01 16:07:15 +0300

    Update cache name from passaword-v5 to passaword-v6

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ======================================== */
 
 /* Önbellek adı ve versiyonu */
-const ONBELLEK_ADI = 'passaword-v5';
+const ONBELLEK_ADI = 'passaword-v6';
 
 /* Önbelleğe alınacak dosyalar */
 const ONBELLEKLENECEK = [
