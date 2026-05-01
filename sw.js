09946d6086cd095670bdb84bf089cabe4cf5b100
berat-arsln <beratarsln@gmail.com>
2026-05-01 19:07:10 +0300

    Update cache name from passaword-v8 to passaword-v9

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ======================================== */
 
 /* Önbellek adı ve versiyonu */
-const ONBELLEK_ADI = 'passaword-v8';
+const ONBELLEK_ADI = 'passaword-v9';
 
 /* Önbelleğe alınacak dosyalar */
 const ONBELLEKLENECEK = [
