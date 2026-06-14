525c5bc50fb3c368d8121fef9206e2ecd33f73f9
berat-arsln <beratarsln@gmail.com>
2026-06-14 11:26:02 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,14 @@ if (window.matchMedia('(display-mode: fullscreen)').matches ||
     document.body.style.overflowY = 'auto';
     document.body.style.overscrollBehaviorY = 'auto';
 }
+let startY = 0;
+document.addEventListener('touchstart', e => {
+    startY = e.touches[0].clientY;
+});
+document.addEventListener('touchend', e => {
+    const endY = e.changedTouches[0].clientY;
+    const diff = endY - startY;
+    if (diff > 80 && window.scrollY === 0) {
+        window.location.reload();
+    }
+});
