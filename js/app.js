6e69bc54395b340007b70724fdaa48af7afe04e8
berat-arsln <beratarsln@gmail.com>
2026-06-14 12:03:21 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ if (isPWA) {
     document.addEventListener('touchend', e => {
         const endY = e.changedTouches[0].clientY;
         const diff = endY - startY;
-        if (diff > 240 && window.scrollY === 0) {
+        if (diff > 800 && window.scrollY === 0) {
             window.location.reload();
         }
     });
