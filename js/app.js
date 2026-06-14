322122e530e4f8750ecfcbf4e42ae3d53c48a5da
berat-arsln <beratarsln@gmail.com>
2026-06-14 11:17:06 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // Sadece PWA modunda pull-to-refresh'e izin ver
 if (window.matchMedia('(display-mode: fullscreen)').matches || 
     window.matchMedia('(display-mode: standalone)').matches) {
+    document.body.style.overflowY = 'auto';
     document.body.style.overscrollBehaviorY = 'auto';
 }
