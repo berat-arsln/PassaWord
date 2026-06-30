7d290226c9d6d37e20ecf66fd466a5a3efc2db40
berat-arsln <beratarsln@gmail.com>
2026-06-30 06:46:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ======================================== */
 
 /* Önbellek adı ve versiyonu */
-const ONBELLEK_ADI = 'passaword-v2.5.42';
+const ONBELLEK_ADI = 'passaword-v2.5.43';
 
 const ONBELLEKLENECEK = [
   './',
